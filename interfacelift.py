9f31691075402a4aca38816bd44061ac4d76bf06
Ory Band <oryband@gmail.com>
2014-02-06 19:33:52 +0200

    Added random sleep, and fixed lots of bugs. Still WIP.

--- interfacelift.py
+++ interfacelift.py
@@ -5,11 +5,13 @@ import requests
 import re
 
 from BeautifulSoup import BeautifulSoup as bs
+from random import randint, seed
+from time import sleep
 
-SAVE_DIR       = 'wallpapers' #Path to download to
+
+SAVE_DIR       = 'wallpapers'
 RESOLUTION     = '1920x1080'
 STOP_IF_EXISTS = True  # Set to False to download all files even if the file exists and True to stop when it finds where it left off.
-MAX_PAGES      = 99999
 
 
 def get_backgrounds():
@@ -21,7 +23,7 @@ def get_backgrounds():
     path = os.path.abspath(SAVE_DIR)
 
     c = 0
-    for page in range(1, MAX_PAGES):
+    for page in xrange(1, 99999):
         downloaded, carry_on = get_images_from_page(base % (RESOLUTION, page), user_agent, pattern, path)
         c += downloaded
         if not carry_on:
@@ -31,27 +33,38 @@ def get_backgrounds():
 
 def get_images_from_page(url, user_agent, pattern, path):
     soup = bs(requests.get(url, headers={'User-Agent': user_agent}).text)
-    c = 0
-    for link in soup.findAll('a', href=pattern):
-        href = 'http://interfacelift.com%s' % link['href']
-        wallpaper = href[href.rfind('/')+1:]
-        save_to = '%s/%s' % (path, wallpaper)
-        exists = os.path.isfile(save_to)
+    wallpapers = soup.findAll('a', href=pattern)
 
-        if STOP_IF_EXISTS and '_%s' % RESOLUTION in href and exists:
-            print "%s already downloaded, stopping." % wallpaper
-            return c, False
-        elif '_%s' % RESOLUTION in href and not exists:
-            print wallpaper
-            r = requests.get(href, headers={'User-Agent': user_agent})
-            with open(save_to, 'wb') as f:
-                f.write(r.content)
-            c += 1
+    # Stop if we no wallpapers on page.
+    if not wallpapers:
+        print 'Reached an empty page, stopping.'
+        return 0, False
+    else:
+        c = 0
+        for link in wallpapers:
+            href = 'http://interfacelift.com%s' % link['href']
+            wallpaper = href[href.rfind('/')+1:]
+            save_to = '%s/%s' % (path, wallpaper)
+            exists = os.path.isfile(save_to)
 
+            if exists and STOP_IF_EXISTS:
+                print "%s already downloaded, stopping." % wallpaper
+                return c, False
+            elif not exists:
+                r = requests.get(href, headers={'User-Agent': user_agent})
+                with open(save_to, 'wb') as f:
+                    f.write(r.content)
+                c += 1
+                print wallpaper
+
+            sleep(randint(5, 10))
+
+    sleep(randint(5, 10))
     return c, True
 
 
 if __name__ == '__main__':
+    seed()
     c = get_backgrounds()
     print '\n%s' % (lambda c: 'Downloaded %d new wallpaper%s.' % (
         c, (lambda c: 's' if c>1 else '')(c))
