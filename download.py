3f35811a4e3f8596c6c1d47fe5eb73c6eae4126f
Ory Band <oryband@gmail.com>
2014-02-07 17:48:36 +0200

    Wallpaper downloads using a single session now; Iterating pages using a lazy list; Code documentation and cosmetic changes; Everything is ready.

--- download.py
+++ download.py
@@ -4,38 +4,56 @@ import os.path
 import requests
 import re
 
-from BeautifulSoup import BeautifulSoup as bs
+from BeautifulSoup import BeautifulSoup
+from itertools import count
 from random import randint, seed
 from time import sleep
 
 
-SAVE_DIR       = 'wallpapers'
-RESOLUTION     = '1920x1080'
-STOP_IF_EXISTS = True  # Set to False to download all files even if the file exists and True to stop when it finds where it left off.
+SAVE_DIR          = 'wallpapers'
+RESOLUTION        = '1920x1080'
+RESOLUTION_PREFIX = 'widescreen'  # URL Prefix for filtering by resolution.
+STOP_IF_EXISTS    = True  # Set to False to download all files even if the file exists and True to stop when it finds where it left off.
+START_PAGE        = 1  # Page number to start downloading from. Keep this on '1' if you want to download everything at the current resolution.
 
 
 def get_backgrounds():
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
-    base = 'http://interfacelift.com/wallpaper/downloads/date/widescreen/%s/index%s.html'
-    user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'
-    pattern = re.compile(r'/wallpaper/.*jpg')
+
     path = os.path.abspath(SAVE_DIR)
 
-    c = 0
-    for page in xrange(1, 99999):
-        downloaded, carry_on = get_images_from_page(base % (RESOLUTION, page), user_agent, pattern, path)
-        c += downloaded
-        if not carry_on:
-            break;
+    pattern = re.compile(r'/wallpaper/.*jpg')
+
+    # Sometimes interfacelift closes the connection, so we have to reconnect.
+    session = requests.Session()
+    adapter = requests.adapters.HTTPAdapter(max_retries=3)
+    session.mount('http://', adapter)
+
+    # Interfacelift blocks non-browser requests.
+    session.headers = {'User-Agent':  'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3 (.NET CLR 3.5.30729)'}
+
+    try:
+        c = 0
+        for page in count(START_PAGE):
+            downloaded, carry_on = get_images_from_page(
+                'http://interfacelift.com/wallpaper/downloads/date/%s/%s/index%s.html' % (
+                    RESOLUTION_PREFIX, RESOLUTION, page),
+                 session, pattern, path)
+            c += downloaded
+            if not carry_on:
+                break;
+    finally:
+        session.close()
 
     return c
 
-def get_images_from_page(url, user_agent, pattern, path):
-    soup = bs(requests.get(url, headers={'User-Agent': user_agent}).text)
+
+def get_images_from_page(url, session, pattern, path):
+    soup = BeautifulSoup(session.get(url).text)
     wallpapers = soup.findAll('a', href=pattern)
 
-    # Stop if we no wallpapers on page.
+    # Stop if no wallpapers on page.
     if not wallpapers:
         print 'Reached an empty page, stopping.'
         return 0, False
@@ -51,21 +69,24 @@ def get_images_from_page(url, user_agent, pattern, path):
                 print "%s already downloaded, stopping." % wallpaper
                 return c, False
             elif not exists:
-                r = requests.get(href, headers={'User-Agent': user_agent})
+                response = session.get(href)
                 with open(save_to, 'wb') as f:
-                    f.write(r.content)
+                    f.write(response.content)
                 c += 1
                 print wallpaper
 
-            sleep(randint(5, 10))
+            # Random break between wallpapers.
+            sleep(randint(2, 5))
 
-    sleep(randint(5, 10))
+    sleep(randint(2, 5))
     return c, True
 
 
 if __name__ == '__main__':
     seed()
     c = get_backgrounds()
+
+    # Summary
     print '\n%s' % (lambda c: 'Downloaded %d new wallpaper%s.' % (
         c, (lambda c: 's' if c>1 else '')(c))
         if c>0 else 'No new wallpapers were downloaded.')(c)
